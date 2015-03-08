f4041500dbe4bedc42e18325853c57235c0ff14d
Aviva Buller <avivabuller@gmail.com>
2015-03-08 07:31:34 +0000

    fix configuration (add configuration 2)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,10 +30,10 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
-    authentication: "plain,"
-    enable_startttls_auto: true,
+    authentication: "plain",
+    enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secret.email_provider_password
+    password: Rails.application.secrets.email_provider_password
     }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
