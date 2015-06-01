013120f95fbd97b19b36938f4e5c4a9cf031726a
Aviva Buller <avivabuller@gmail.com>
2015-06-01 04:08:36 +0000

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,9 @@
 class VisitorsController < ApplicationController
+  
   def new
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
   end
   
 end
